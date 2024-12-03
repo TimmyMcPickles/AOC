836ae8febc3395265b3498797d7bff7c3d3b6dd4
Ethan <kingscout1011@gmail.com>
2024-12-02 19:36:06 -0600

    formatted code to make it look better, and hopefully more readable.

--- Day-1/Part-1/main.cpp
+++ Day-1/Part-1/main.cpp
@@ -1,25 +1,27 @@
 #include <algorithm>
 #include <fstream>
-#include <vector>
 #include <iostream>
+#include <vector>
 
-int main(){
-    std::ifstream file("../input.txt");
-    std::vector<int> list1, list2;
-    int x, y;
-    int distance = 0;
+int main() {
+  std::ifstream file("../input.txt");
+  std::vector<int> list1, list2;
+  int x, y;
+  int distance = 0;
 
-    while (file >> x >> y){
-        list1.push_back(x);
-        list2.push_back(y);
-    }
-    std::sort(list1.begin(), list1.end());
-    std::sort(list2.begin(), list2.end());
-    for (int i = 0; i < list1.size(); i++){
-        distance += abs(list1[i] - list2[i]);
-    }
+  while (file >> x >> y) {
+    list1.push_back(x);
+    list2.push_back(y);
+  }
 
-    std::cout << distance << std::endl;
+  std::sort(list1.begin(), list1.end());
+  std::sort(list2.begin(), list2.end());
 
-    return 0;
+  for (int i = 0; i < list1.size(); i++) {
+    distance += abs(list1[i] - list2[i]);
+  }
+
+  std::cout << distance << std::endl;
+
+  return 0;
 }
--- Day-1/Part-2/main.cpp
+++ Day-1/Part-2/main.cpp
@@ -1,38 +1,43 @@
 
 #include <algorithm>
 #include <fstream>
-#include <vector>
 #include <iostream>
+#include <vector>
 
-int main(){
-    std::ifstream file("../input.txt");
-    std::vector<int> list1, list2;
-    int x, y;
+int main() {
+  std::ifstream file("../input.txt");
+  std::vector<int> list1, list2;
+  int x, y;
 
-    int total_duplicates = 0;
-    int sim = 0;
-    //list1 = {3, 4, 2, 1, 3, 3};
-    //list2 = {4, 3, 5, 3, 9, 3};
+  int total_duplicates = 0;
+  int sim = 0;
+  // list1 = {3, 4, 2, 1, 3, 3};
+  // list2 = {4, 3, 5, 3, 9, 3};
 
-    while (file >> x >> y){
-        list1.push_back(x);
-        list2.push_back(y);
-    }
-    std::sort(list1.begin(), list1.end());
-    std::sort(list2.begin(), list2.end());
-    for (int i = 0; i < list1.size(); i++){
-        total_duplicates = 0;
-        int j = 0;
-        while (j < list2.size() && list1[i] > list2[j]){
-            j++;
-        }
-        while (j < list2.size() && list1[i] == list2[j]){   // This code was mostly borrowed from reddit
-            total_duplicates ++;                            // Thanks to ednl in the day 1 discussion
-            j++;                                            //thread https://www.reddit.com/r/adventofcode/comments/1h44xmb/comment/lzw5k8j/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
-        }
-        sim += list1[i] * total_duplicates;
+  while (file >> x >> y) {
+    list1.push_back(x);
+    list2.push_back(y);
+  }
+
+  std::sort(list1.begin(), list1.end());
+  std::sort(list2.begin(), list2.end());
+
+  for (int i = 0; i < list1.size(); i++) {
+    total_duplicates = 0;
+    int j = 0;
+    while (j < list2.size() && list1[i] > list2[j]) {
+      j++;
     }
 
-    std::cout << sim << std::endl;
-    return 0;
+    while (j < list2.size() &&
+           list1[i] == list2[j]) { // This code was mostly borrowed from reddit
+      total_duplicates++;          // Thanks to ednl in the day 1 discussion
+      j++;                         // thread
+           // https://www.reddit.com/r/adventofcode/comments/1h44xmb/comment/lzw5k8j/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+    }
+    sim += list1[i] * total_duplicates;
+  }
+
+  std::cout << sim << std::endl;
+  return 0;
 }
